3d1bac793f71370e57dee4e7551cbe3f65f54b46
ashwin17893 <ashwin.kolurmath@bluedanube.com>
2018-04-25 20:37:17 -0700

    added movie adding and hall adding api's

--- routes/operations.js
+++ routes/operations.js
@@ -113,4 +113,124 @@ router.post('/signup', function (req, res, next) {
         })
  });
 });
+
+router.post('/addmovies', function (req, res, next) {
+    console.log("in addmovies");
+
+    var title = req.body.title;
+    var trailer = req.body.trailer;
+    var cast = req.body.cast;
+    var user_id =  Math.floor(Math.random() * Math.floor(9999));
+    var release_date = req.body.release_date;
+    var rating = req.body.rating;
+    var photos = req.body.photos;
+    var length = req.body.length;
+    var theatres = req.body.theatres;
+    var reviews = req.body.reviews;
+
+    var data = {
+        title : title,
+        trailer : trailer,
+        cast : cast,
+        release_date: release_date,
+        rating: rating,
+        photos: photos,
+        length: length,
+        theatres: theatres,
+        reviews: reviews
+
+
+    }
+
+    mongo.connect(function(db){
+        console.log("Connected to MongoDB at ",url)
+
+        var coll = db.collection('addmovie');
+        coll.findOne({'title':title},function (err,user) {
+            if(err){
+                console.log("sending status 401")
+                res.json({
+                    status : false
+                });
+            }
+            else if(user){
+                console.log("sending status 401")
+                res.json({
+                    status : false
+                });
+            }
+
+            else{
+                mongo.insertDocument(db,'addmovie',data,function (err,results) {
+                    if (err) {
+                        console.log("sending status 401")
+                        res.json({
+                            status: false
+                        });
+                    }
+                    else {
+                        console.log("Movie added successfully")
+                        var path = results["ops"][0]["_id"];
+                        console.log(path);
+                        res.json({
+                            status: true,
+                        });
+                    }
+                });
+            }
+        })
+    });
+});
+
+
+router.post('/addhall', function (req, res, next) {
+    console.log("in addhall");
+
+    var time1 = req.body.time1;
+    var time2 = req.body.time2;
+    var time3 = req.body.time3;
+    var user_id =  Math.floor(Math.random() * Math.floor(9999));
+    var time4 = req.body.time4;
+    var time5 = req.body.time5;
+    var tickets = req.body.tickets;
+    var screen = req.body.screen;
+    var price = req.body.price;
+
+    var data = {
+        time1 : time1,
+        time2 : time2,
+        time3 : time3,
+        time4: time4,
+        time5: time5,
+        tickets: tickets,
+        screen: screen,
+        price: price
+
+
+    }
+
+    mongo.connect(function(db){
+        console.log("Connected to MongoDB at ",url)
+
+                mongo.insertDocument(db,'addhall',data,function (err,results) {
+                    if (err) {
+                        console.log("sending status 401")
+                        res.json({
+                            status: false
+                        });
+                    }
+                    else {
+                        console.log("Movie hall added successfully")
+                        var path = results["ops"][0]["_id"];
+                        console.log(path);
+                        res.json({
+                            status: true,
+                        });
+                    }
+                });
+
+
+    });
+});
+
 module.exports = router;
